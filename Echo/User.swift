82f9b6b1753abd9fe31909dc6c34c675f47e59c5
Isis Anchalee <isiswenger@gmail.com>
2016-03-05 01:39:27 -0800

    Update PFUser properties

--- Echo/User.swift
+++ Echo/User.swift
@@ -33,12 +33,39 @@ class User: NSObject {
             if ((error) != nil) {
                 print("Error: \(error)")
             } else {
-                self.facebook_id = result.valueForKey("id") as? String
-                self.username = result.valueForKey("name") as? String
-                self.email = result.valueForKey("email") as? String
-                self.profilePhotoUrl = "https://graph.facebook.com/\(self.facebook_id)/picture?width=300&height=300"
-                self.coverPhotoUrl = "https://graph.facebook.com/\(FBSDKAccessToken.currentAccessToken().userID)/cover?"
+                var responseDict: [String: String]! = Dictionary<String,String>()
+                let id: String? = result.valueForKey("id") as? String
+                    self.facebook_id = id!
+                responseDict["facebook_id"] = id!
+                responseDict["username"] = result.valueForKey("name") as? String
+                responseDict["email"] =  result.valueForKey("email") as? String
+                responseDict["profilePhotoUrl"] = "https://graph.facebook.com/\(self.facebook_id)/picture?width=300&height=300"
+                responseDict["coverPhotoUrl"] = "https://graph.facebook.com/\(FBSDKAccessToken.currentAccessToken().userID)/cover?"
+                self.saveLocally(responseDict)
+                self.saveToParse(responseDict)
             }
         })
+    }
+    
+    func saveLocally(result: NSDictionary){
+        self.facebook_id = result.valueForKey("id") as? String
+        self.username = result.valueForKey("name") as? String
+        self.email = result.valueForKey("email") as? String
+        self.profilePhotoUrl = "https://graph.facebook.com/\(self.facebook_id)/picture?width=300&height=300"
+        self.coverPhotoUrl = "https://graph.facebook.com/\(FBSDKAccessToken.currentAccessToken().userID)/cover?"
+    }
+    
+    func buildDictionary(result: NSObject) -> [String: String] {
+        var responseDict: [String: String]!
+        responseDict["facebook_id"] = self.facebook_id!
+        responseDict["username"] = self.username!
+        responseDict["email"] =  self.email!
+        responseDict["profilePhotoUrl"] = self.profilePhotoUrl!
+        responseDict["coverPhotoUrl"] = self.coverPhotoUrl!
+        return responseDict
+    }
+    
+    func saveToParse(dict: NSDictionary){
+        ParseClient.sharedInstance.setCurrentUserWithDict(dict)
     }
 }
