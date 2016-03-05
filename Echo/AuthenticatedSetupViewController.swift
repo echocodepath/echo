0daeaf811939bd57f51b28fdcda2303741d0c9c8
Isis Anchalee <isiswenger@gmail.com>
2016-03-04 16:21:28 -0800

    Add parse API client

--- Echo/AuthenticatedSetupViewController.swift
+++ Echo/AuthenticatedSetupViewController.swift
@@ -15,11 +15,31 @@ class AuthenticatedSetupViewController: UIViewController {
     @IBAction func onLogout(sender: AnyObject) {
         let loginManager = FBSDKLoginManager()
         loginManager.logOut()
-
+        openLoginPage()
+    }
+    
+    func openLoginPage() {
         let loginViewController = self.storyboard!.instantiateViewControllerWithIdentifier("LoginViewController") as! LoginViewController
         let loginNav = UINavigationController(rootViewController: loginViewController)
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         appDelegate.window?.rootViewController = loginNav
+    }
+    
+    func openHomePage() {
+        let homeViewController = self.storyboard!.instantiateViewControllerWithIdentifier("HomeViewController") as! HomeViewController
+        let homePageNav = UINavigationController(rootViewController: homeViewController)
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        appDelegate.window?.rootViewController = homePageNav
+    }
+    
+    @IBAction func onSelectDanceTeacher(sender: AnyObject) {
+        ParseClient.sharedInstance.setUserValue("is_teacher", value: "true")
+        openHomePage()
+    }
+    
+    @IBAction func onSelectDanceStudent(sender: AnyObject) {
+        ParseClient.sharedInstance.setUserValue("is_teacher", value: "false")
+        openHomePage()
     }
     
     override func viewDidLoad() {
