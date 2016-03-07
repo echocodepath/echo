f22a513dea0ede6682876e899c7c445ef3fd92b8
Christine Hong <christinehong802@yahoo.com>
2016-03-07 12:04:36 -0800

    Add feedback request flow

---
+++ Echo/TeacherFeedbackCell.swift
@@ -0,0 +1,26 @@
+//
+//  TeacherFeedbackCell.swift
+//  Echo
+//
+//  Created by Christine Hong on 3/7/16.
+//  Copyright © 2016 echo. All rights reserved.
+//
+
+import UIKit
+
+class TeacherFeedbackCell: UITableViewCell {
+    
+    @IBOutlet weak var teacherName: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
