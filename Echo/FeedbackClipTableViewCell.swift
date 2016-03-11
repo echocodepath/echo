348b85b4e8d0ed022b9b5c20193c0f8ae92d255e
Isis Anchalee <isiswenger@gmail.com>
2016-03-11 14:27:59 -0800

    Pass entry data to record feedback form

---
+++ Echo/FeedbackClipTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedbackClipTableViewCell.swift
+//  Echo
+//
+//  Created by Isis Anchalee on 3/11/16.
+//  Copyright © 2016 echo. All rights reserved.
+//
+
+import UIKit
+
+class FeedbackClipTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Echo/InboxDetailsViewController.swift
+++ Echo/InboxDetailsViewController.swift
@@ -64,14 +64,18 @@ class InboxDetailsViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if let identifier = segue.identifier {
+            switch identifier {
+            case "AcceptFeedbackSegue":
+                let navController = segue.destinationViewController as! UINavigationController
+                let acceptFeedbackRequestViewController = navController.topViewController as! AcceptFeedbackRequestViewController
+                acceptFeedbackRequestViewController.entry = currentEntry
+            default:
+                return
+            }
+        }
+        
     }
-    */
 
 }
