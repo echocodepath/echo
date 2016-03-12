0d73c4d7360fc34be6094ce96e5d65c4a1365f9a
Isis Anchalee <isiswenger@gmail.com>
2016-03-11 18:51:24 -0800

    Working custom slider

--- Echo/AcceptFeedbackRequestViewController.swift
+++ Echo/AcceptFeedbackRequestViewController.swift
@@ -16,14 +16,21 @@ class AcceptFeedbackRequestViewController: UIViewController {
     var entry: PFObject?
     let controller = AVPlayerViewController()
     var player: AVPlayer?
+    var timeObserver: AnyObject!
+    var playerRateBeforeSeek: Float = 0
     
+    @IBOutlet weak var timeLeftLabel: UILabel!
     @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var timeSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         if entry != nil {
+            timeSlider.value = 0.0
             convertVideoDataToNSURL()
+            bindRecordOptions()
         }
+        
     }
 
     @IBAction func onTogglePlayPause(sender: AnyObject) {
@@ -47,6 +54,13 @@ class AcceptFeedbackRequestViewController: UIViewController {
     func bindRecordOptions() {
         recordButton.addTarget(self, action:"handleTouchUp:", forControlEvents: .TouchUpInside)
         recordButton.addTarget(self, action: "handleTouchDown:", forControlEvents: .TouchDown)
+        
+        timeSlider.addTarget(self, action: "sliderBeganTracking:",
+            forControlEvents: UIControlEvents.TouchDown)
+        timeSlider.addTarget(self, action: "sliderEndedTracking:",
+            forControlEvents: [UIControlEvents.TouchUpInside, UIControlEvents.TouchUpOutside])
+        timeSlider.addTarget(self, action: "sliderValueChanged:",
+            forControlEvents: UIControlEvents.ValueChanged)
     }
     
     func handleTouchUp() {
@@ -57,6 +71,32 @@ class AcceptFeedbackRequestViewController: UIViewController {
     func handleTouchDown() {
         // pause video
         // start recording audio
+    }
+    
+    func sliderBeganTracking(slider: UISlider) {
+        print("sliderBeganTracking")
+        playerRateBeforeSeek = player!.rate
+        player!.pause()
+    }
+    
+    func sliderEndedTracking(slider: UISlider) {
+        print("sliderEndedTracking")
+        let videoDuration = CMTimeGetSeconds(player!.currentItem!.duration)
+        let elapsedTime: Float64 = videoDuration * Float64(timeSlider.value)
+        updateTimeLabel(elapsedTime: elapsedTime, duration: videoDuration)
+        
+        player!.seekToTime(CMTimeMakeWithSeconds(elapsedTime, 10)) { (completed: Bool) -> Void in
+            if (self.playerRateBeforeSeek > 0) {
+                self.player!.play()
+            }
+        }
+    }
+    
+    func sliderValueChanged(slider: UISlider) {
+        print("sliderValueChanged")
+        let videoDuration = CMTimeGetSeconds(player!.currentItem!.duration)
+        let elapsedTime: Float64 = videoDuration * Float64(timeSlider.value)
+        updateTimeLabel(elapsedTime: elapsedTime, duration: videoDuration)
     }
     
     private func playVideo(url: NSURL){
@@ -72,6 +112,14 @@ class AcceptFeedbackRequestViewController: UIViewController {
         controller.view.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor).active = true
         controller.view.heightAnchor.constraintEqualToAnchor(controller.view.widthAnchor, multiplier: 1, constant: 1)
         player = AVPlayer(URL: url)
+        let playerItem = AVPlayerItem(URL: url)
+        player!.replaceCurrentItemWithPlayerItem(playerItem)
+        let timeInterval: CMTime = CMTimeMakeWithSeconds(1.0, 10)
+        timeObserver = player!.addPeriodicTimeObserverForInterval(timeInterval,
+            queue: dispatch_get_main_queue()) { (elapsedTime: CMTime) -> Void in
+                self.observeTime(elapsedTime)
+        }
+        
         controller.player = player
         controller.player!.play()
     }
@@ -88,7 +136,24 @@ class AcceptFeedbackRequestViewController: UIViewController {
             
         }
     }
-
+    
+    private func updateTimeLabel(elapsedTime elapsedTime: Float64, duration: Float64) {
+        let timeRemaining: Float64 = elapsedTime
+        timeSlider.value = Float(elapsedTime/100)
+        timeLeftLabel.text = String(format: "%02d:%02d", ((lround(timeRemaining) / 60) % 60), lround(timeRemaining) % 60)
+    }
+    
+    private func observeTime(elapsedTime: CMTime) {
+        let duration = CMTimeGetSeconds(player!.currentItem!.duration);
+        if (isfinite(duration)) {
+            let elapsedTime = CMTimeGetSeconds(elapsedTime)
+            updateTimeLabel(elapsedTime: elapsedTime, duration: duration)
+        }
+    }
+    
+    deinit {
+        player!.removeTimeObserver(timeObserver)
+    }
     /*
     // MARK: - Navigation
 
