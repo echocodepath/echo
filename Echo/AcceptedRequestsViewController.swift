01853ca394e22dccc871410edc39326d7d14b1c2
Christine Hong <christinehong802@yahoo.com>
2016-03-14 22:29:26 -0700

    Add ability to reject requests

--- Echo/AcceptedRequestsViewController.swift
+++ Echo/AcceptedRequestsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  InboxTabBarController.swift
+//  AcceptedRequestsViewController.swift
 //  Echo
 //
 //  Created by Christine Hong on 3/14/16.
@@ -8,23 +8,12 @@
 
 import UIKit
 
-class InboxTabBarController: UITabBarController {
-    @IBOutlet weak var inboxTabBar: UITabBar!
-    @IBAction func onBack(sender: AnyObject) {
-        self.dismissViewControllerAnimated(true, completion: nil)
-    }
-
+class AcceptedRequestsViewController: UIViewController {
+    @IBOutlet weak var acceptedRequestsTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let tabItems = self.inboxTabBar.items! as [UITabBarItem]
-        let tabItem0 = tabItems[0] as UITabBarItem
-        let tabItem1 = tabItems[1] as UITabBarItem
-        let tabItem2 = tabItems[2] as UITabBarItem
-        let tabItem3 = tabItems[3] as UITabBarItem
-        tabItem0.title = "Inbox"
-        tabItem1.title = "Sent"
-        tabItem2.title = "Accepted"
-        tabItem3.title = "Rejected"
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
