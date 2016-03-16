e4ba83cade96d65999f424355ac78a5819111a69
Isis Anchalee <isiswenger@gmail.com>
2016-03-16 16:10:30 -0700

    Fix back button navigationcontroller

--- Echo/EntryTableViewCell.swift
+++ Echo/EntryTableViewCell.swift
@@ -26,7 +26,7 @@ class EntryTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         self.backgroundColor = UIColor.clearColor()
-        self.contentView.backgroundColor = UIColor(red: 245.0/255.0, green: 245.0/255.0, blue: 245.0/255.0, alpha: 0.4)
+        self.contentView.backgroundColor = UIColor(red: 245.0/255.0, green: 245.0/255.0, blue: 245.0/255.0, alpha: 0.3)
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
