a2fb88c41c6deafaf02861c51ce000ba56b70b06
Andrew <yu.drew@gmail.com>
2016-03-19 11:29:14 -0700

    Add functionality, time out to navigate out of request sent page

--- Echo/FeedbackRequestSentViewController.swift
+++ Echo/FeedbackRequestSentViewController.swift
@@ -14,11 +14,29 @@ class FeedbackRequestSentViewController: UIViewController {
     
     @IBOutlet weak var teacherLabel: UILabel!
     
+    @IBAction func onTapAnywhere(sender: AnyObject) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc  = storyboard.instantiateViewControllerWithIdentifier("HomeNavigationController") as! UINavigationController
+        self.presentViewController(vc, animated: true, completion: nil)
+    }
+    
+    func navToHome(){
+        print("Nav to home invoked")
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc  = storyboard.instantiateViewControllerWithIdentifier("HomeNavigationController") as! UINavigationController
+        self.presentViewController(vc, animated: true, completion: nil)
+        
+    }
+    
     private var teacherName : String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.teacherLabel.text = self.teacherName
+        
+        let timer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: "navToHome", userInfo: nil, repeats: true)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,12 +46,6 @@ class FeedbackRequestSentViewController: UIViewController {
     
     func setTeacher(teacher: PFObject) {
         self.teacherName = teacher["username"] as? String
-    }
-    
-    @IBAction func returnHome(sender: AnyObject) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc  = storyboard.instantiateViewControllerWithIdentifier("HomeNavigationController") as! UINavigationController
-        self.presentViewController(vc, animated: true, completion: nil)
     }
 
     /*
