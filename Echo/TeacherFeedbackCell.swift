6244de2c894a217c0593041a157737bac774577f
Isis Anchalee <isiswenger@gmail.com>
2016-03-21 21:07:02 -0700

    Polish entry page

--- Echo/TeacherFeedbackCell.swift
+++ Echo/TeacherFeedbackCell.swift
@@ -37,8 +37,9 @@ class TeacherFeedbackCell: UITableViewCell {
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        let view = UIView()
+        view.backgroundColor = StyleGuide.Colors.echoCellSelectedMint
+        selectedBackgroundView = view
     }
 
 }
