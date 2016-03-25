845205645633729055afbaf3d0f3f61618cc8afd
Isis Anchalee <isiswenger@gmail.com>
2016-03-25 00:14:43 -0700

    Finish table view glory on entry page

---
+++ Echo/VideoCell.swift
@@ -0,0 +1,15 @@
+
+//
+//  VideoCell.swift
+//  Echo
+//
+//  Created by Isis Anchalee on 3/24/16.
+//  Copyright © 2016 echo. All rights reserved.
+//
+
+import UIKit
+
+class VideoCell: UITableViewCell {
+
+    @IBOutlet weak var videoContainer: UIView!
+}
