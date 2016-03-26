75c728cbdaec8d314825cd0a36f4f53f5ca8b898
Isis Anchalee <isiswenger@gmail.com>
2016-03-25 17:49:15 -0700

    Working form on feedback details

--- Echo/FeedbackRequestDetailsViewController.swift
+++ Echo/FeedbackRequestDetailsViewController.swift
@@ -24,7 +24,6 @@ class FeedbackRequestDetailsViewController: UITableViewController, UITextViewDel
     var teacher: PFObject?
     var videoId: String?
     
-    var controller: AVPlayerViewController?
     
     @IBOutlet weak var videoContainerView: UIView!
     @IBOutlet weak var formBackgroundView: UIView!
@@ -256,7 +255,7 @@ class FeedbackRequestDetailsViewController: UITableViewController, UITextViewDel
         if let id = videoId {
             FileProcessor.sharedInstance.deleteVideoFileWithId(id)
         }
-        controller!.player?.pause()
+        videoPlayer.player?.pause()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -279,7 +278,7 @@ class FeedbackRequestDetailsViewController: UITableViewController, UITextViewDel
                 case "feedbackSentSegue":
                     let vc = segue.destinationViewController as! FeedbackRequestSentViewController
                     vc.setTeacher(self.teacher!)
-                    controller!.player?.pause()
+                    videoPlayer.player?.pause()
                     sendFeedbackRequest()
                     
                 default:
