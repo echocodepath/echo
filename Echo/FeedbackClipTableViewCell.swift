067159ec4a09e7a77a91a44bd58ec0a7dd55298a
Isis Anchalee <isiswenger@gmail.com>
2016-03-30 00:00:50 -0700

    Make sound things work and look sexy

--- Echo/FeedbackClipTableViewCell.swift
+++ Echo/FeedbackClipTableViewCell.swift
@@ -45,6 +45,8 @@ class FeedbackClipTableViewCell: UITableViewCell {
         timestampLabel.textColor = StyleGuide.Colors.echoTranslucentClear
         noteLabel.textColor = UIColor(red: 199/255, green: 161/255, blue: 129/255, alpha: 1.0)
         noteLabel.text = "NOTE \(FeedbackClipTableViewCell.count)"
+        
+        selectionStyle = .None
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
